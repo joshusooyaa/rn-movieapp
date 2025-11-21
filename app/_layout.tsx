1a54e7071e1df056e74b2caafffaf103aac7a8b6
Josh Sawyer <106642518+joshusooyaa@users.noreply.github.com>
2025-11-21 03:34:25 -0800

    Fix header

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,7 @@
 import { Stack } from "expo-router";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import './globals.css';
 
 export default function RootLayout() {
-  return <Stack />;
+  return <Stack />
 }
